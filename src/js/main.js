c30f586d8e2663fe583f06b8577a8cb0d5797b25
Lonwaid <Andrey9053@yandex.ru>
2019-10-07 20:38:14 +0300

    Добавлено модальное окно на jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,12 @@ button.addEventListener('click', function() {
 	modal.classList.add('modal_active');
 	// console.log('');
 });
+button.addEventListener('click', function() {
+	setTimeout(second_passed, 5000);
+});	
 close.addEventListener('click', function() {
 	modal.classList.remove('modal_active');
 });
+function second_passed() {
+	modal.classList.remove('modal_active');
+};
